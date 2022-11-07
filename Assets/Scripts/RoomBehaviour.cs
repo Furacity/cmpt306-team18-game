755180db5b5cac14967b1901b870e942ff136f49
Noah V <62227085+Furacity@users.noreply.github.com>
2022-11-06 23:40:13 -0600

    Started adjusting the PRG

--- Assets/Scripts/RoomBehaviour.cs
+++ Assets/Scripts/RoomBehaviour.cs
@@ -17,4 +17,58 @@ public class RoomBehaviour : MonoBehaviour
             walls[i].SetActive(!status[i]);
         }
     }
+
+    public void UpdateRotation(bool[] direction)
+    {
+        if(tag == "endroom")
+        {
+            // only room with 1 opening is the end room, rotate it so it faces towards the direction given
+            // default is right
+            if (direction[0])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, -90, transform.rotation.z));
+            }
+
+            if (direction[1])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 90, transform.rotation.z));
+            }
+
+            if (direction[2])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
+            }
+
+            if (direction[3])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 180, transform.rotation.z));
+            }
+
+
+            // we dont need an else because default entrance faces east
+        }
+        else if (tag == "hallway")
+        {
+            if (direction[0] && direction[1])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
+            }
+            if (direction[2] && direction[3])
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 90, transform.rotation.z));
+            }
+        }
+        else if (tag == "Lroom")
+        {
+
+        }
+        else if (tag == "Troom")
+        {
+
+        }
+        else if (tag == "4wayroom")
+        {
+
+        }
+    }
 }
