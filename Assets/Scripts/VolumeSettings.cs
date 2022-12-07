b6e0bb1833c954152903c4c0a18f27352de0bee3
Noah V <62227085+Furacity@users.noreply.github.com>
2022-12-07 00:10:16 -0600

    Added values to slider text

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -11,6 +11,10 @@ public class VolumeSettings : MonoBehaviour
     public Slider musicSlider;
     public Slider SFXSlider;
     public Slider UISlider;
+    public Text masterLabel;
+    public Text musicLabel;
+    public Text SFXLabel;
+    public Text UILabel;
     public AudioSource buttonForward;
     public AudioSource buttonBack;
     public AudioSource sliderChange;
@@ -37,22 +41,26 @@ public class VolumeSettings : MonoBehaviour
     public void OnMasterSliderChange()
     {
         masterVol = masterSlider.value;
+        masterLabel.text = Mathf.Floor(masterSlider.value * 100) + "%";
         sliderChange.Play();
     }
 
     public void OnMusicSliderChange()
     {
         musicVol = musicSlider.value;
+        musicLabel.text = Mathf.Floor(musicSlider.value * 100) + "%";
         sliderChange.Play();
     }
     public void OnSFXSliderChange()
     {
         SFXVol = SFXSlider.value;
+        SFXLabel.text = Mathf.Floor(SFXSlider.value * 100) + "%";
         sliderChange.Play();
     }
     public void OnUISliderChange()
     {
         UIVol = UISlider.value;
+        UILabel.text = Mathf.Floor(UISlider.value * 100) + "%";
         sliderChange.Play();
     }
 
@@ -80,15 +88,18 @@ public class VolumeSettings : MonoBehaviour
         groups[0].audioMixer.GetFloat("MasterVol", out masterVol);
         masterVol = Mathf.Pow(10, masterVol / 20);
         masterSlider.value = masterVol;
+        masterLabel.text = Mathf.Floor(masterSlider.value * 100) + "%";
         groups[1].audioMixer.GetFloat("MusicVol", out musicVol);
         musicVol = Mathf.Pow(10, musicVol / 20);
         musicSlider.value = musicVol;
+        musicLabel.text = Mathf.Floor(musicSlider.value * 100) + "%";
         groups[2].audioMixer.GetFloat("SFXVol", out SFXVol);
         SFXVol = Mathf.Pow(10, SFXVol / 20);
         SFXSlider.value = SFXVol;
+        SFXLabel.text = Mathf.Floor(SFXSlider.value * 100) + "%";
         groups[3].audioMixer.GetFloat("UIVol", out UIVol);
         UIVol = Mathf.Pow(10, UIVol / 20);
-        Debug.Log(UIVol);
         UISlider.value = UIVol;
+        UILabel.text = Mathf.Floor(UISlider.value * 100) + "%";
     }
 }
